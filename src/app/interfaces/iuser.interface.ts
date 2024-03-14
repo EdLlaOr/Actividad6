b6246ce30bb743b9cca3bf0411f0cffa47f04294
Eduardo Llaria Orbegozo <edllaor77@gmail.com>
2024-03-14 17:12:15 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/interfaces/iuser.interface.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    email: string;
+    first_name: string; 
+    id: number;
+    image: string;
+    last_name: string;
+    password: string;
+    username: string;
+    _id: string; 
+}
